9bc04f706a41ff5116829d44985e0562c231af27
Paul Thimote <pthimote@gmail.com>
2022-09-15 15:31:58 -0400

    commit tree 20f1bb1e6669ca2634bfe08015271227cc693066

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce(
+    (batteryBatches, totalBatteries) => batteryBatches + totalBatteries
+)
+
+// const array1 = [1, 2, 3, 4];
+
+// const initialValue = 0;
+// const sumWithInitial = array1.reduce(
+//   (previousValue, currentValue) => previousValue + currentValue,
+//   initialValue
+// );
+
+// console.log(sumWithInitial);
+// // expected output: 10
